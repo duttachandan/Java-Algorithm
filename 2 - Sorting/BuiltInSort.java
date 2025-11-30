56973a041e9f85bc14fba2cc7c990271eddc2d26
duttachandan <duttaankit917@gmail.com>
2025-11-30 08:54:54 +0530

    updated built in method sort

---
+++ 2 - Sorting/BuiltInSort.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class BuiltInSort {
+    public static void main(String args[]) {
+        int arr[] = { 3, 2, 1, 4, 6, 6 };
+        Arrays.sort(arr);
+        System.out.print(Arrays.toString(arr));
+    }
+}
