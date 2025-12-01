6ce95b4159488009a48aa47d023552838b80761a
duttachandan <duttaankit917@gmail.com>
2025-12-01 16:22:38 +0530

    updated Sprial Matrix

--- 3 - 2D Arrays/SpiralMatrix.java
+++ 3 - 2D Arrays/SpiralMatrix.java
@@ -6,22 +6,54 @@ public class SpiralMatrix {
         int EndRow = arg.length - 1;
         int EndCol = arg[0].length - 1;
         while (startRow <= EndRow && startCol <= EndCol) {
-            for (int j = 0; j <= arg[0].length - 1; j++) {
+            // System.err.println(EndCol > startCol);
+            // System.err.println(EndRow > startRow);
+
+            // if (EndRow == startRow && EndCol == startCol) {
+            //     System.err.print(arg[startRow][EndRow]);
+            //     break;
+            // }
+
+            for (int j = startCol; j <= EndCol; j++) {
                 System.out.print(arg[startRow][j]);
             }
-            System.out.println(" ");
-            for (int i = 1; i < arg.length; i++) {
+            for (int i = startRow + 1; i <= EndRow; i++) {
                 System.out.print(arg[i][EndCol]);
             }
+            for (int i = EndCol - 1; i >= startCol; i--) {
+                System.out.print(arg[EndRow][i]);
+            }
+            for (int j = EndRow - 1; j >= startRow + 1; j--) {
+                System.out.print(arg[j][startCol]);
+            }
+
+            startRow++;
+            startCol++;
+            EndCol--;
+            EndRow--;
+            // System.err.println(" ");
+            // System.out.println(EndRow + " " + startRow + " " + EndCol + " " + startCol);
+            // System.out.println(" ");
+
         }
     }
 
     public static void main(String args[]) {
+
         int arg[][] = {
-            { 3, 2, 4 },
-            { 2, 4, 6 },
-            { 1, 3, 5 }
+                { 3, 2, 4 },
+                { 2, 4, 6 },
+                { 1, 3, 5 }
+        };
+
+        int arg2[][] = {
+            {4, 6, 7, 8},
+            {1, 3, 4, 5},
+            {9, 2, 3, 1},
+            {4, 8, 6, 2}
         };
         SpiralPrint(arg);
+        System.err.println();
+        SpiralPrint(arg2);
     }
 }
