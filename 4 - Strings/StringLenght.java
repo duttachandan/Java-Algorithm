d8cefc229e5982bd60790b508a3c87a852c5acb1
duttachandan <duttaankit917@gmail.com>
2025-12-02 18:26:56 +0530

    Updated String

---
+++ 4 - Strings/StringLenght.java
@@ -0,0 +1,7 @@
+public class StringLenght {
+    public static void main(String[] args) {
+        String firstWrord = "I am a string value";
+        System.out.print(firstWrord.charAt(8));
+        System.err.println(firstWrord.length());
+    }
+}
